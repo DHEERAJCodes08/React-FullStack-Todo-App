d328703b5c1528320a04e9cd98003107c476ed92
unknown <you@example.com>
2024-10-27 02:39:45 +0530

    Initial commit of frontend and backend folders

--- Server/db.js
+++ Server/db.js
@@ -1,10 +1,10 @@
 const {Client} = require('pg')
 
 const pool = new Client({
-    user:'postgres',
-    password:'dheerajprasad',
-    host:'localhost',
-    database :'React Project'
+    user:'process.env.DB_USER',
+    password:'process.env.DB_PASS',
+    host:'process.env.DB_HOST',
+    database :'process.env.DB_DATABASE',
 })
 
 module.exports = pool;  
